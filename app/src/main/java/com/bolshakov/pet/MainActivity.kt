26adfe27158d1fbc5f1581c7db9db5ed697e16df
bolshakovk <bolshakovk1234@gmail.com>
2022-07-17 00:50:02 +0300

    fix lint

--- app/src/main/java/com/bolshakov/pet/MainActivity.kt
+++ app/src/main/java/com/bolshakov/pet/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.bolshakov.pet
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
