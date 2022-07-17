abb9024d33222d06c64194d5a6dabd3f0eec589d
bolshakovk <bolshakovk1234@gmail.com>
2022-07-17 18:51:29 +0300

    huh?2master

--- app/src/main/java/com/bolshakov/pet/SignUpFragment.kt
+++ app/src/main/java/com/bolshakov/pet/SignUpFragment.kt
@@ -1,10 +1,11 @@
 package com.bolshakov.pet
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
 class SignUpFragment : Fragment() {
 
@@ -15,7 +16,8 @@ class SignUpFragment : Fragment() {
     private lateinit var viewModel: SignUpViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
